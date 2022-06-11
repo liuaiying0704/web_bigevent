34c6c467b55e09620d9db2c1cd7839a9a9bb4707
liuaiying <1425577366@qq.com>
2022-06-11 09:54:37 +0800

    完成index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -11,4 +11,16 @@ $.ajaxPrefilter((options) => {
       Authorization: localStorage.getItem('token'),
     };
   }
+  options.complete = (res) => {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      //  1、强制清空 token
+      localStorage.removeItem('token');
+      // 2、强制跳转到登录页面
+      location.href = '/login.html';
+    }
+  };
 });
